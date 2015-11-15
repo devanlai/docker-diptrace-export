5a9f9f7ee54b8412193ac8261c089b853cec700a
Devan Lai <devan.lai@gmail.com>
2015-11-14 19:20:24 -0800

    Use stdin/stdout instead of volumes
    Instead of using volumes, the schematic is now passed in through stdin
    and the exported images are zipped and passed out through stdout
    
    Note: due to docker peculiarities, the "-i" flag must be used with
    "docker run"
    
    Example usage:
    docker run -i --rm devanlai/diptrace-export < test.dch > test.zip

--- Dockerfile
+++ Dockerfile
@@ -14,5 +14,4 @@ RUN rm /tmp/diptrace.reg
 
 COPY ahk/*.ahk /tmp/ahk/
 
-VOLUME ["/mnt/import/", "/mnt/export/"]
 ENTRYPOINT ["/tmp/bin/export-bitmap.sh"]
--- bin/export-bitmap.sh
+++ bin/export-bitmap.sh
@@ -1,11 +1,20 @@
 #!/bin/sh
-IMPORT_PATH_WIN=`winepath -w "$1"`
-AHK_PATH_WIN="C:\Program Files\AutoHotkey\AutoHotkey.exe"
-DT_PATH=`winepath "C:\Program Files\DipTrace\Schematic.exe"`
+TEMP_SCHEMATIC_NAME="/tmp/export.dch"
+cat - > /tmp/export.dch
+DIR_NAME=${1:-schematic}
+IMPORT_PATH_WIN=`winepath -w "$TEMP_SCHEMATIC_NAME" 2>/dev/null`
+AHK_PATH_WIN=`winepath /tmp/bin/AutoHotkey.exe 2>/dev/null`
+DT_PATH=`winepath "C:\Program Files\DipTrace\Schematic.exe" 2>/dev/null`
 WINE_PATH=`which wine`
-DPI=${DPI:-300}
+EXP_DPI=${DPI:-300}
+SCH_PATH=$IMPORT_PATH_WIN
 
-WINE_PATH="$WINE_PATH" DT_PATH="$DT_PATH" SCH_PATH="$IMPORT_PATH_WIN" \
-EXP_DPI=$DPI xvfb-run wine "$AHK_PATH_WIN" /tmp/ahk/schematic-to-bmp.ahk
+export WINE_PATH DT_PATH SCH_PATH EXP_DPI
+xvfb-run wine "$AHK_PATH_WIN" /tmp/ahk/schematic-to-bmp.ahk 2>/dev/null 1>/dev/null
 
-/tmp/bin/waitfor.sh wineserver && mv ~/DipTrace/*.bmp $2/;
+/tmp/bin/waitfor.sh wineserver
+
+mkdir -p /tmp/export/$DIR_NAME
+cp ~/DipTrace/*.bmp /tmp/export/$DIR_NAME
+cd /tmp/export/
+zip - $DIR_NAME $DIR_NAME/* 2>/dev/null
